6d7840c7d26e1e51f9d273d4634dd2a23d9fcfeb
choiyoonho <cyh7834@gmail.com>
2022-03-23 21:30:03 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop2/domain/item/Item.java
+++ src/main/java/jpabook/jpashop2/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop2.domain.item;
 
 import jpabook.jpashop2.domain.Category;
+import jpabook.jpashop2.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 }
--- src/test/java/jpabook/jpashop2/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop2/service/MemberServiceTest.java
@@ -1,7 +1,7 @@
-package jpabook.jpashop2.domain.service;
+package jpabook.jpashop2.service;
 
 import jpabook.jpashop2.domain.Member;
-import jpabook.jpashop2.domain.repository.MemberRepository;
+import jpabook.jpashop2.repository.MemberRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
